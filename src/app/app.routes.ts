238f165e7bbcefc08cb75b666d35ee3ee3fe2071
soyjherom <jherom@tecnica.co.cr>
2025-12-25 09:53:23 -0600

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,63 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/public/public.component').then(
+        (m) => m.PublicComponent
+      ),
+      children: [
+        {
+          path: '',
+          pathMatch: 'full',
+          loadComponent: () =>
+            import('./features/home/home.component').then(
+              (m) => m.HomeComponent
+            )
+        },
+        {
+          path: 'products',
+          loadComponent: () =>
+            import('./features/products/products.component').then(
+              (m) => m.ProductsComponent
+            )
+        },
+        {
+          path: 'cart',
+          loadComponent: () =>
+            import('./features/cart/cart.component').then(
+              (m) => m.CartComponent
+            )
+        },
+        {
+          path: 'checkout',
+          loadComponent: () =>
+            import('./features/checkout/checkout.component').then(
+              (m) => m.CheckoutComponent
+            )
+        }
+      ]
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./layouts/admin/admin.component').then(
+        (m) => m.AdminComponent
+      ),
+      children: [
+        {
+          path: '',
+          pathMatch: 'full',
+          loadComponent: () =>
+            import('./features/admin/admin.component').then(
+              (m) => m.AdminComponent
+            )
+        }
+      ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
